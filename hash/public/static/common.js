9054ae12f8509c54e76e32e452c25b3f83b64ce0
rehellinen <912377791@qq.com>
2017-10-12 20:52:36 +0800

    111

---
+++ hash/public/static/common.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rehellinen on 2017/10/12.
+ */
+$('#detail').click(function () {
+
+
+
+        layer.open({
+            type: 2,
+            title: '座位查看',
+            maxmin: true,
+            area: ['800px', '500px'],
+            content: "http://rehellinen.cn/fix/hash/public/index.php?s=index/index/seat"
+        });
+
+
+
+    });
+
